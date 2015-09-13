55ef5aca107bcc499293863146ef6a84604a88da
Sanggyu Nam <pokeplus@gmail.com>
2015-09-13 21:17:54 +0900

    Define functions for finding prime numbers

--- src/prime_numbers.c
+++ src/prime_numbers.c
@@ -1,29 +1,62 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void* t_func(void* index) {
-  printf("Running thread #%d...\n", *(int*) index);
-  return NULL;
-}
+/*
+ * Finds prime numbers between a and b, stores them in prime_numbers (only if
+ * prime_numbers is not NULL), and returns the number of found prime numbers.
+ */
+size_t find_prime_numbers(const unsigned long a,
+                          const unsigned long b,
+                          unsigned long** prime_numbers);
+/*
+ * Prints the list of prime numbers. The array of prime numbers and the number
+ * of them should be provided.
+ */
+void print_prime_numbers(const unsigned long* prime_numbers,
+                         const size_t n_prime);
 
 int main(void) {
-  int i;
-  int t_indices[10];
-  pthread_t threads[10];
-
-  for (i = 0; i < 10; ++i) {
-    t_indices[i] = i + 1;
-    pthread_create(&threads[i], NULL, t_func, (void*) &t_indices[i]);
-  }
-
-  for (i = 0; i < 10; ++i) {
-    void* ret;
-    pthread_join(threads[i], (void**) &ret);
-  }
-
-  puts("Done.");
-
+  /* Lower bound for finding prime numbers. */
+  const unsigned long a = 1;
+  /* Upper bound for finding prime numbers. */
+  const unsigned long b = 1000000000;  /* 10^9 */
+  /* The number of prime numbers in the range. */
+  size_t n_prime;
+  /* The list of found prime numbers. */
+  unsigned long* prime_numbers;
+  n_prime = find_prime_numbers(a, b, &prime_numbers);
+  print_prime_numbers(prime_numbers, n_prime);
+  printf("Total number of prime numbers between %lu and %lu is %lu.\n",
+         a, b, n_prime);
+  /* Set them free. */
+  free(prime_numbers);
   return 0;
+}
+
+size_t find_prime_numbers(const unsigned long a,
+                          const unsigned long b,
+                          unsigned long** prime_numbers) {
+  size_t n_prime = 0;
+  int store_numbers = !(prime_numbers == NULL);
+  if (store_numbers) {
+    /*
+     * Allocate space for the list of prime numbers. The number of prime
+     * numbers is unknown here. Therefore, allocate space for the half of
+     * numbers between a and b.
+     */
+    *prime_numbers =
+        (unsigned long*) malloc((b - a) / 2 * sizeof(unsigned long));
+  }
+  return n_prime;
+}
+
+void print_prime_numbers(const unsigned long* prime_numbers,
+                         const size_t n_prime) {
+  size_t i;
+  for (i = 0; i < n_prime; ++i) {
+    printf("%lu\n", prime_numbers[i]);
+  }
 }

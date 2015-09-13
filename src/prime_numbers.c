63bd3da74644e40b8d27fb1ae07d65c4ea2a6a78
Sanggyu Nam <pokeplus@gmail.com>
2015-09-13 15:54:24 +0900

    Update indent spaces from 4 to 2

--- src/prime_numbers.c
+++ src/prime_numbers.c
@@ -4,26 +4,26 @@
 #include <unistd.h>
 
 void* t_func(void* index) {
-    printf("Running thread #%d...\n", *(int*) index);
-    return NULL;
+  printf("Running thread #%d...\n", *(int*) index);
+  return NULL;
 }
 
 int main(void) {
-    int i;
-    int t_indices[10];
-    pthread_t threads[10];
+  int i;
+  int t_indices[10];
+  pthread_t threads[10];
 
-    for (i = 0; i < 10; ++i) {
-        t_indices[i] = i + 1;
-        pthread_create(&threads[i], NULL, t_func, (void*) &t_indices[i]);
-    }
+  for (i = 0; i < 10; ++i) {
+    t_indices[i] = i + 1;
+    pthread_create(&threads[i], NULL, t_func, (void*) &t_indices[i]);
+  }
 
-    for (i = 0; i < 10; ++i) {
-        void* ret;
-        pthread_join(threads[i], (void**) &ret);
-    }
+  for (i = 0; i < 10; ++i) {
+    void* ret;
+    pthread_join(threads[i], (void**) &ret);
+  }
 
-    puts("Done.");
+  puts("Done.");
 
-    return 0;
+  return 0;
 }
